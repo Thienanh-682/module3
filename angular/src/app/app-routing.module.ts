41d38238a46cde80cc983f6c701c5eb647b66175
Thienanh-682 <Thienanh-682@gmail.com>
2019-11-26 11:46:45 +0700

    CRUD

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ProductListComponent} from './product/product-list/product-list.component';
-import {ProductAddComponent} from './product/product-add/product-add.component';
-import {ProductEditComponent} from './product/product-edit/product-edit.component';
 
 
 const routes: Routes = [
-  { path: 'products', component: ProductListComponent},
-  { path: 'products/create', component: ProductAddComponent},
-  { path: 'products/:id/edit', component: ProductEditComponent}
+  { path: 'products', loadChildren: './product/product.module#ProductModule'},
+
 ];
 
 @NgModule({
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './core/menu/menu.component';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { StarComponent } from './star/star/star.component';
-import { ProductAddComponent } from './product/product-add/product-add.component';
-import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import {ProductModule} from './product/product.module';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ProductListComponent,
-    StarComponent,
-    ProductAddComponent,
-    ProductEditComponent
   ],
   imports: [
     BrowserModule,
+    ProductModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
